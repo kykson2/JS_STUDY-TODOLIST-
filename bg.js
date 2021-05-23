a6163d21b6278a0ae9a60dbf34debc4f4c4baa16
GWANWOO <ganwoo98@gmail.com>
2021-05-23 23:23:58 +0900

    second commit

---
+++ bg.js
@@ -0,0 +1,24 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 3;
+
+
+function paintImage(imgNumber){
+    const img = new Image();
+    img.src = `./images/${imgNumber+1}.jpg`;
+    img.classList.add("bgImage");
+    body.prepend(img);
+    
+}
+
+function genRandom(){
+    const number = Math.floor(Math.random() * IMG_NUMBER);
+    return number;
+}
+
+function init(){
+    const randomNumber = genRandom();
+    paintImage(randomNumber);
+}
+
+init();
